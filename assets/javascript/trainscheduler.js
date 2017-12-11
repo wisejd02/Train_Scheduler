fdf34a6e1956f90b38c7386b147081f18fe4659a
Joshua <Joshua@Admins-MacBook.local>
2017-12-11 17:33:12 -0600

    modified edit and delete buttons to work correctly

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -15,7 +15,6 @@
      var trainFreq = "";
 
      database.ref("train_schedule").orderByChild("dateAdded").on("child_added", function(response) {
-         var dbUID;
          var tFrequency = response.val().trainFreq;
          var firstTimeConverted = moment(response.val().firstTrain, "hh:mm").subtract(1, "years");
          // Current Time
@@ -28,11 +27,11 @@
          var tMinutesTillTrain = tFrequency - tRemainder;
          // Next Train
          var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("HH:mm");
-
+         
          $("tbody").append(`
              <tr id="${response.key}">
-                <td><button class="btn btn-edit btn-upd">Edit</button>
-                <button class="btn btn-edit btn-del onclick="onClickDelete('')">Delete</button></td>
+                <td><button id="btn_${response.key}_upd" class="btn btn-edit" onclick="updateRow(this)">Edit</button>
+                <button id="btn_${response.key}_del" class="btn btn-edit" onclick="deleteRow(this)">Delete</button></td>
                 <td>${response.val().trainName}</td>
                 <td>${response.val().trainDest}</td>
                 <td>${response.val().firstTrain}</td>
@@ -40,56 +39,51 @@
                 <td>${nextTrain}</td>
                 <td>${tMinutesTillTrain}</td>
              </tr>`)
-        $("button.btn-upd").click( function(){
-            console.log("Edit");
-            //console.log($(this).parents('tr')[0]);
-            var currentTD = $(this).parents('tr').find('td');
-          if ($(this).html() == 'Edit') {
-              currentTD = $(this).parents('tr').find('td');
-              console.log(currentTD);
-              
-              $.each(currentTD, function (index, val) {
-                  console.log(this);
-                  console.log(index);
-                  if(index != 0 && index < 5){
-                    $(this).prop('contenteditable', true)
-                  }
-                  
-              });
-          } else {
-             $.each(currentTD, function () {
-                 console.log("save should be")
-                  $(this).prop('contenteditable', false)
-              });
-              dbUID = $(this).parents('tr')[0].id;
-              var updateTrainData = {
-                trainName: $(this).parents('tr')[0].cells[1].innerHTML.trim(),
-                 trainDest: $(this).parents('tr')[0].cells[2].innerHTML.trim(),
-                firstTrain: moment($(this).parents('tr')[0].cells[3].innerHTML.trim(), "HHmm").format("HH:mm"),
-                 trainFreq: $(this).parents('tr')[0].cells[4].innerHTML.trim(),
-                 dateAdded: firebase.database.ServerValue.TIMESTAMP
-              }
-              database.ref("train_schedule/"+dbUID).set(updateTrainData);
-          }
-
-          $(this).html($(this).html() == 'Edit' ? 'Save' : 'Edit')
-        });
-        $(".btn-del").click( function(){
-            dbUID = $(this).parents('tr')[0].id;
-            console.log("delete");
-            console.log($(this).parents('tr'));
-            $(this).parents('tr')[0].remove()
-            database.ref("train_schedule/"+dbUID).remove()
-        });
      });
+     function updateRow(obj){
+        var currentRow = $(obj).parents('tr')[0].id;
+        if ($(obj).html() == 'Edit') {
+            $('#'+currentRow).each(function() {
+                $.each(this.cells, function(index, val){
+                    if(index != 0 && index <5){
+                        $(this).prop('contenteditable', true)
+                    }
+                    
+                });
+            
+            });
+        }else {
+            $('#'+currentRow).each(function() {
+                $.each(this.cells, function () {
+                    $(this).prop('contenteditable', false)
+                });
+            });
+            var dbUID = $(obj).parents('tr')[0].id;
+            var updateTrainData = {
+                trainName: $(obj).parents('tr')[0].cells[1].innerHTML.trim(),
+                trainDest: $(obj).parents('tr')[0].cells[2].innerHTML.trim(),
+                firstTrain: moment($(obj).parents('tr')[0].cells[3].innerHTML.trim(), "HHmm").format("HH:mm"),
+                trainFreq: $(obj).parents('tr')[0].cells[4].innerHTML.trim(),
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            }
+            database.ref("train_schedule/"+dbUID).set(updateTrainData);
 
+        }
+        $(obj).html($(obj).html() == 'Edit' ? 'Save' : 'Edit')
+    };
+
+    function deleteRow(obj){
+        var dbUID = $(obj).parents('tr')[0].id;
+        $(obj).parents('tr')[0].remove()
+        database.ref("train_schedule/"+dbUID).remove()
+    };
 
      $("#btnFrmSubmit").click( function(e){
          e.preventDefault();
          var formData = {
              trainName: $("#trainName").val().trim(),
              trainDest: $("#trainDest").val().trim(),
-            firstTrain: moment($("#firstTrain").val().trim(), "HHmm").format("HH:mm"),
+             firstTrain: moment($("#firstTrain").val().trim(), "HHmm").format("HH:mm"),
              trainFreq: $("#trainFreq").val().trim(),
              dateAdded: firebase.database.ServerValue.TIMESTAMP
          };
@@ -100,17 +94,11 @@
          });
          // database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
          // 	var sv = snapshot.val();
-         // 	console.log(sv);
 
 
          // });
      });
      
-
-     $("tr").click( function(){
-         console.log("tr");
-         console.log(this);
-     });
      function updateTrain(obj){
         database.ref("train_schedule").push(obj);
 
